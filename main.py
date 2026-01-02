064cc2e79304be985584686b7128a361554200cb
Dalveer Singh <dalveersingh9994@gmail.com>
2026-01-02 19:41:25 +0530

    new file:   harvard.wav 	new file:   main.py 	new file:   process.txt 	new file:   sample_voice test telugu.wav

---
+++ main.py
@@ -0,0 +1,40 @@
+import whisper
+from pathlib import Path
+
+AUDIO_PATH = "C:/Users/dalve/Desktop/E2E project/23774605_Sampradayam.mp3"   
+WHISPER_MODEL = "turbo"           
+
+OUTPUT_DIR = Path("outputs")
+OUTPUT_DIR.mkdir(exist_ok=True)
+
+TELUGU_FILE = OUTPUT_DIR / "telugu.txt"
+ENGLISH_FILE = OUTPUT_DIR / "english.txt"
+
+model = whisper.load_model(WHISPER_MODEL, device="gpu")
+print(f"Using device: {model.device}")
+
+#Telugu audio → Telugu text
+telugu_result = model.transcribe(
+    AUDIO_PATH,
+    language="te",
+    task="transcribe"
+)
+
+telugu_text = telugu_result["text"].strip()
+TELUGU_FILE.write_text(telugu_text, encoding="utf-8")
+
+# English audio → English text
+# Telugu audio → English text
+english_result = model.transcribe(
+    AUDIO_PATH,
+    task="translate",   # always outputs English
+    language="en"
+)
+
+english_text = english_result["text"].strip()
+ENGLISH_FILE.write_text(english_text, encoding="utf-8")
+
+print("Completed successfully.")
+print("Saved files:")
+print(TELUGU_FILE)
+print(ENGLISH_FILE)
